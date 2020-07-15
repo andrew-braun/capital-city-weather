303f2ba28c45c1e3430c21cb6a7e6e6c7c936762
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-07-15 19:16:45 +0400

    added keys to rows

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -4,22 +4,22 @@ import Row from "../components/Row";
 
 const DataTable = ({ geoData }) => {
   const cityRows = Object.entries(geoData).map(([key, value], i) => {
-    return <Row rowData={value} rowNumber={i} />;
+    return <Row rowData={value} rowNumber={i} key={`${value.city}-${value.country}`}/>;
   });
   return (
     <article className="data-container">
       <table className="data-table">
         <thead className="table-header">
           <tr>
-            <th key="city-header" className="city-header">City</th>
-            <th key="country-header" className="country-header">Country</th>
-            <th key="continent-header" className="continent-header">Continent</th>
-            <th key="date-header" className="date-header">Date</th>
-            <th key="time-header" className="time-header">Time</th>
-            <th key="temp-header" className="temp-header">Temp</th>
-            <th key="weather-header" className="weather-header">Weather</th>
-            <th key="humidity-header" className="humidity-header">Humidity</th>
-            <th key="windspeed-header" className="windspeed-header">Windspeed</th>
+            <th className="city-header">City</th>
+            <th className="country-header">Country</th>
+            <th className="continent-header">Continent</th>
+            <th className="date-header">Date</th>
+            <th className="time-header">Time</th>
+            <th className="temp-header">Temp</th>
+            <th className="weather-header">Weather</th>
+            <th className="humidity-header">Humidity</th>
+            <th className="windspeed-header">Windspeed</th>
           </tr>
         </thead>
         <tbody>{cityRows}</tbody>
