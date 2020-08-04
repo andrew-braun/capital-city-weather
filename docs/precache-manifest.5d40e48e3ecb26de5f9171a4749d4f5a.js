0c5e0e6144ccb17e54a14e529f8403f17a9b17ab
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-08-04 16:19:45 +0400

    added docs folder for Github Pages

---
+++ docs/precache-manifest.5d40e48e3ecb26de5f9171a4749d4f5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b539c6338bb31eeb1176deb1582e2e1",
+    "url": "/capital-city-weather/index.html"
+  },
+  {
+    "revision": "2e0642d42902f1b45975",
+    "url": "/capital-city-weather/static/css/main.7ac74128.chunk.css"
+  },
+  {
+    "revision": "1c6e4f365ccc14b896e2",
+    "url": "/capital-city-weather/static/js/2.3506b416.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/capital-city-weather/static/js/2.3506b416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e0642d42902f1b45975",
+    "url": "/capital-city-weather/static/js/main.1df611a0.chunk.js"
+  },
+  {
+    "revision": "f4eb2a0ddbc24e11fe36",
+    "url": "/capital-city-weather/static/js/runtime-main.4d6da2e2.js"
+  }
+]);
